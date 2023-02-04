d5f77ff0827821d28e62a1620828e7b10a223a63
szaccagni <sarahzaccagni@gmail.com>
2023-02-04 12:40:47 -0500

    adds about, records index and basic styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+records = [
+    {'artist': 'Mac Miller', 'album': 'Circles', 'released': 'May 29, 2020'},
+    {'artist': 'Nirvana', 'album': 'Incesticide', 'released': 'December 15, 1992'},
+    {'artist': 'Queen', 'album': 'A Day at the Races', 'released': 'December 10, 1976'},
+    {'artist': 'David Bowie', 'album': 'Hunky Dory', 'released': 'December 17, 1971'}
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def records_index(request):
+    return render(request, 'records/index.html', {
+        'records': records
+    })
